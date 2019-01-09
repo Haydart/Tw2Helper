21e660f3b4f7e06b5ce2ce9132a9a504485ac0a2
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-09 01:26:16 +0100

    Rewrite resource distribution with promises

--- bot/distributeResources.js
+++ bot/distributeResources.js
@@ -1,111 +1,118 @@
-const NOT_SENT_RESOURCES_CAPACITY = 0.3;
+module.exports = {
+    sendResources: (_socketService, _routeProvider, _plainVillages, _academyVillages) =>
+        sendResourcesIfViable(_socketService, _routeProvider, _plainVillages, _academyVillages)
+};
 
+let socketService = null;
+let routeProvider = null;
+let plainVillages = null;
+let academyVillages = null;
 
-function fetchOwnVillagesInfo(andThenAction) {
-    (function getOwnVillages() {
-        socketService.emit(
-            routeProvider.GET_CHARACTER_VILLAGES,
-            {},
-            response => {
-                onOwnVillagesFetched(response, andThenAction);
-            })
-    })();
-}
+const NOT_SENT_RESOURCES_CAPACITY = 0.6;
 
-function onOwnVillagesFetched(response, andThenAction) {
-    console.log(response);
-    user.academyVillages = [];
-    user.plainVillages = [];
+function sendResourcesIfViable(socketService, routeProvider, plainVillages, academyVillages) {
+    this.socketService = socketService;
+    this.routeProvider = routeProvider;
+    this.plainVillages = plainVillages;
+    this.academyVillages = academyVillages;
 
-    response.villages.forEach((village) => {
-        if (village.academy) {
-            user.academyVillages.push(village)
-        } else {
-            user.plainVillages.push(village)
-        }
+    let resourcePromises = [];
+
+    plainVillages.forEach(plainVillage => {
+        resourcePromises.push(new Promise(resolve => {
+            fetchAvailableMerchantsCount(plainVillage)
+                .then(availableMerchantsCount => {
+                    if (availableMerchantsCount > 0) {
+                        return sendResourcesToClosestAcademyVillage(plainVillage, availableMerchantsCount)
+                    } else {
+                        return new Promise.resolve()
+                    }
+                })
+                .then(resourceSendingResponse => {
+                    console.log("Finished resources sending process to village " + plainVillage.name);
+                    resolve(resourceSendingResponse)
+                })
+        }));
     });
 
-    console.log("Academy villages " + user.academyVillages.length);
-    console.log("Plain villages " + user.plainVillages.length);
-
-    andThenAction();
-}
-
-function sendResourcesIfViable() {
-    user.plainVillages.forEach((village) => {
-        fetchAvailableMerchantsCount(village)
+    return new Promise((resolve, reject) => {
+        Promise.all(resourcePromises)
+            .then(response => resolve(response))
     })
 }
 
 function fetchAvailableMerchantsCount(village) {
-    socketService.emit(
-        routeProvider.TRADING_GET_MERCHANT_STATUS,
-        {
-            village_id: village.id,
-        },
-        response => {
-            onAvailableMerchantsCountFetched(village, response.free)
-        })
-}
-
-function onAvailableMerchantsCountFetched(village, availableMerchantsCount) {
-    if (availableMerchantsCount > 0) {
-        sendResourcesToClosestAcademyVillage(village, availableMerchantsCount)
-    } else {
-        console.log("VILLAGE " + village.name + " HAS NO AVAILABLE MERCHANTS")
-    }
+    return new Promise((resolve, reject) => {
+        this.socketService.emit(
+            this.routeProvider.TRADING_GET_MERCHANT_STATUS,
+            {
+                village_id: village.id,
+            },
+            response => {
+                resolve(response.free)
+            })
+    });
 }
 
 function sendResourcesToClosestAcademyVillage(village, availableMerchantsCount) {
-    let minDistanceToAcademyVillage = Number.MAX_SAFE_INTEGER;
-    let closestAcademyVillageId = undefined;
+    return new Promise((resolve, reject) => {
+        let minDistanceToAcademyVillage = Number.MAX_SAFE_INTEGER;
+        let closestAcademyVillageId = null;
 
-    user.academyVillages.forEach((academyVillage) => {
-        let distanceFromCurrentAcademyVillage = Math.sqrt(
-            Math.pow(village.x - academyVillage.x, 2) + Math.pow(village.y - academyVillage.y, 2)
-        );
-        if (distanceFromCurrentAcademyVillage < minDistanceToAcademyVillage) {
-            minDistanceToAcademyVillage = distanceFromCurrentAcademyVillage;
-            closestAcademyVillageId = academyVillage.id;
+        this.academyVillages.forEach(academyVillage => {
+            let distanceFromCurrentAcademyVillage = Math.sqrt(
+                Math.pow(village.x - academyVillage.x, 2) + Math.pow(village.y - academyVillage.y, 2)
+            );
+            if (distanceFromCurrentAcademyVillage < minDistanceToAcademyVillage) {
+                minDistanceToAcademyVillage = distanceFromCurrentAcademyVillage;
+                closestAcademyVillageId = academyVillage.id;
+            }
+        });
+
+        let transportCapacity = availableMerchantsCount * 1000;
+        let villageMaxStorage = village.res_max_storage;
+
+        let woodAmount = Math.floor(Math.min(
+            Math.max(village.res_wood - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
+            Math.floor(transportCapacity / 3)
+        ));
+        let clayAmount = Math.floor(Math.min(
+            Math.max(village.res_clay - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
+            Math.floor(transportCapacity / 3)
+        ));
+        let ironAmount = Math.floor(Math.min(
+            Math.max(village.res_iron - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
+            Math.floor(transportCapacity / 3)
+        ));
+
+        if (woodAmount > 0 || clayAmount > 0 || ironAmount > 0) {
+            performSendResources(village, closestAcademyVillageId, woodAmount, clayAmount, ironAmount)
+                .then(response => {
+                    resolve(response)
+                })
+        } else {
+            console.log("VILLAGE " + village.name + " HAS NO SPARE RESOURCES");
+            resolve()
         }
     });
-
-    let transportCapacity = availableMerchantsCount * 1000;
-    let villageMaxStorage = village.res_max_storage;
-
-    let woodAmount = Math.floor(Math.min(
-        Math.max(village.res_wood - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
-        Math.floor(transportCapacity / 3)
-    ));
-    let clayAmount = Math.floor(Math.min(
-        Math.max(village.res_clay - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
-        Math.floor(transportCapacity / 3)
-    ));
-    let ironAmount = Math.floor(Math.min(
-        Math.max(village.res_iron - villageMaxStorage * NOT_SENT_RESOURCES_CAPACITY, 0),
-        Math.floor(transportCapacity / 3)
-    ));
-
-    if (woodAmount > 0 || clayAmount > 0 || ironAmount > 0) {
-        sendResources(village, closestAcademyVillageId, woodAmount, clayAmount, ironAmount)
-    } else {
-        console.log("VILLAGE " + village.name + " HAS NO SPARE RESOURCES")
-    }
 }
 
-function sendResources(startVillage, targetVillageId, woodAmount, clayAmount, ironAmount) {
-    socketService.emit(
-        routeProvider.TRADING_SEND_RESOURCES,
-        {
-            start_village: startVillage.id,
-            target_village: targetVillageId,
-            wood: woodAmount,
-            clay: clayAmount,
-            iron: ironAmount
-        },
-        response => {
-            console.log("VILLAGE " + startVillage.name + " SENT " + woodAmount + " " + clayAmount + " " + ironAmount);
-        })
+function performSendResources(startVillage, targetVillageId, woodAmount, clayAmount, ironAmount) {
+    return new Promise(resolve => {
+        this.socketService.emit(
+            this.routeProvider.TRADING_SEND_RESOURCES,
+            {
+                start_village: startVillage.id,
+                target_village: targetVillageId,
+                wood: woodAmount,
+                clay: clayAmount,
+                iron: ironAmount
+            },
+            response => {
+                console.log("VILLAGE " + startVillage.name + " SENT " + woodAmount + " " + clayAmount + " " + ironAmount);
+                resolve(response)
+            })
+    });
 }
 
 //
