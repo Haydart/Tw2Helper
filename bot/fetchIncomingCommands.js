fb068876c2556bac54b34f0998a25b44ab384a9e
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-12 03:33:50 +0100

    Fetch all incoming attacks

--- bot/fetchIncomingCommands.js
+++ bot/fetchIncomingCommands.js
@@ -12,11 +12,32 @@ function notifyAttack(socketService, routeProvider, allVillages) {
     this.allVillages = allVillages;
 
     let villagesIds = allVillages.map(village => village.id);
-    console.log(villagesIds);
-
-    // fetchIncomingCommands();
 
     return new Promise(resolve => {
-        resolve()
+        fetchIncomingCommands(villagesIds)
+            .then(incomingCommandsResponse => {
+                resolve()
+            })
     });
+}
+
+function fetchIncomingCommands(villagesIds) {
+    return new Promise(resolve => {
+        this.socketService.emit(
+            this.routeProvider.OVERVIEW_GET_INCOMING,
+            {
+                count: 10,
+                offset: 0,
+                sorting: "false",
+                reverse: false,
+                groups: [],
+                command_types: ['attack'],
+                villages: villagesIds
+            },
+            response => {
+                resolve(response);
+                console.log(response)
+            }
+        )
+    })
 }
